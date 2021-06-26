fbdf18e13afdeef6fe2d5deb4eb8dc9beeec5723
Jerald <jeraldssw@gmail.com>
2021-06-26 20:52:08 +0800

    Removed duplicate packages

--- App.js
+++ App.js
@@ -30,7 +30,7 @@ export default function App() {
                 ? (
                     <AuthContextProvider>
                         <NavigationContainer>
-                            <HomeStack />
+                            <RootStack />
                         </NavigationContainer>
                     </AuthContextProvider>
                 )
