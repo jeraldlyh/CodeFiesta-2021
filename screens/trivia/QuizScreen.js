667376415f73a397959a009450b2ce6fef3069ac
Jerald <jeraldssw@gmail.com>
2021-07-03 00:30:32 +0800

    Fix layout padding in trivia

--- screens/trivia/QuizScreen.js
+++ screens/trivia/QuizScreen.js
@@ -56,13 +56,11 @@ function QuizScreen({ navigation }) {
     };
 
     return (
-        <Layout>
-            <Header />
-
+        <Fragment>
             {/* Modal Box */}
             {
                 displayCorrect
-                    ? <BlurView intensity={95} style={[tailwind("items-center justify-center"), { height: "100%", position: 'absolute', width: 500, zIndex: 100 }]}>
+                    ? <BlurView intensity={95} style={[tailwind("items-center justify-center"), { height: "100%", position: 'absolute', width: "100%", zIndex: 100 }]}>
                         <View style={tailwind("flex flex-col items-center justify-center h-56 w-56 bg-white rounded-xl")}>
                             <Text style={[styles.title, tailwind("text-center mb-1 text-xl")]}>Congralutations!</Text>
                             <Text style={[styles.title, tailwind("text-center text-lg mb-3")]}>+75 points</Text>
@@ -76,7 +74,7 @@ function QuizScreen({ navigation }) {
             }
             {
                 displayError
-                    ? <BlurView intensity={95} style={[tailwind("items-center justify-center"), { height: "100%", position: 'absolute', width: 500, zIndex: 100 }]}>
+                    ? <BlurView intensity={95} style={[tailwind("items-center justify-center"), { height: "100%", position: 'absolute', width: "100%", zIndex: 100 }]}>
                         <View style={tailwind("flex flex-col items-center justify-center h-56 w-56 bg-white rounded-xl")}>
                             <Text style={[styles.title, tailwind("text-center mb-1 text-xl")]}>Oh no!</Text>
                             <Text style={[styles.description, tailwind("text-center mb-3")]}>You didn't managed to get the correct answer! 😐</Text>
@@ -87,52 +85,56 @@ function QuizScreen({ navigation }) {
                     </BlurView>
                     : null
             }
-            {
-                question
-                    ?
-                    <Fragment>
-                        <Text style={[styles.title, tailwind("text-2xl mb-4")]}>{question.question}</Text>
-                        <View>
-                            <CountdownCircleTimer
-                                strokeWidth={20}
-                                isPlaying={isPlaying}
-                                duration={5}
-                                colors={[
-                                    ['#004777', 0.4],
-                                    ['#F7B801', 0.4],
-                                    ['#A30000', 0.2],
-                                ]}
-                                onComplete={onComplete}
-                            >
-                                {({ remainingTime, animatedColor }) => (
-                                    <Animated.Text style={[{ color: animatedColor }, tailwind("text-3xl"), styles.description]}>
-                                        {remainingTime}
-                                    </Animated.Text>
-                                )}
-                            </CountdownCircleTimer>
-                        </View>
 
-                        <View style={tailwind("flex flex-col items-center justify-center w-96")}>
-                            <View style={tailwind("h-3 mt-5")} />
-                            {
-                                _.toArray(question.options).map((question, index) => {
-                                    const buttonHeight = question.length > 39 ? 20 : 16
-                                    return (
-                                        <Button key={index} onPress={() => setActiveIndex(index + 1)} text={question} backgroundColor={isActive(index + 1) ? "#FE904B" : "#e3e3e3"} textColor={isActive(index + 1) ? "#FFF" : "#000"} height={buttonHeight} />
-                                    )
-                                })
-                            }
-                        </View>
+            <Layout>
+                <Header />
+                {
+                    question
+                        ?
+                        <Fragment>
+                            <Text style={[styles.title, tailwind("text-2xl mb-4")]}>{question.question}</Text>
+                            <View>
+                                <CountdownCircleTimer
+                                    strokeWidth={20}
+                                    isPlaying={isPlaying}
+                                    duration={5}
+                                    colors={[
+                                        ['#004777', 0.4],
+                                        ['#F7B801', 0.4],
+                                        ['#A30000', 0.2],
+                                    ]}
+                                    onComplete={onComplete}
+                                >
+                                    {({ remainingTime, animatedColor }) => (
+                                        <Animated.Text style={[{ color: animatedColor }, tailwind("text-3xl"), styles.description]}>
+                                            {remainingTime}
+                                        </Animated.Text>
+                                    )}
+                                </CountdownCircleTimer>
+                            </View>
 
-                        <View style={tailwind("mt-5")}>
-                            <TouchableOpacity style={styles.button} onPress={submitButton}>
-                                <Text style={styles.buttonText}>Confirm</Text>
-                            </TouchableOpacity>
-                        </View>
-                    </Fragment>
-                    : <Loading />
-            }
-        </Layout >
+                            <View style={tailwind("flex flex-col items-center justify-center w-96")}>
+                                <View style={tailwind("h-3 mt-5")} />
+                                {
+                                    _.toArray(question.options).map((question, index) => {
+                                        const buttonHeight = question.length > 39 ? 20 : 16
+                                        return (
+                                            <Button key={index} onPress={() => setActiveIndex(index + 1)} text={question} backgroundColor={isActive(index + 1) ? "#FE904B" : "#e3e3e3"} textColor={isActive(index + 1) ? "#FFF" : "#000"} height={buttonHeight} />
+                                        )
+                                    })
+                                }
+                            </View>
+
+                            <View style={tailwind("mt-5")}>
+                                <TouchableOpacity style={styles.button} onPress={submitButton}>
+                                    <Text style={styles.buttonText}>Confirm</Text>
+                                </TouchableOpacity>
+                            </View>
+                        </Fragment>
+                        : <Loading />
+                }
+            </Layout>
+        </Fragment>
     )
 };
 
