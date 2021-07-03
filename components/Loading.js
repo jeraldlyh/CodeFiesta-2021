2ad217dbf1bd3193611977a65cda8a5d77ba148d
Felice <felicepng2010@gmail.com>
2021-07-03 17:38:23 +0800

    Edit lottie

--- components/Loading.js
+++ components/Loading.js
@@ -1,6 +1,8 @@
-import React from "react";
-import { View, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import { Text, View, StyleSheet } from "react-native";
 import BouncingPreloader from "react-native-bouncing-preloaders";
+import Spinner from 'react-native-loading-spinner-overlay';
+import tailwind from "tailwind-rn";
 
 const icons = [
     require("../assets/loading/merli.png"),
@@ -9,23 +11,29 @@ const icons = [
 
 function Loading() {
     return (
-        <View style={styles.container}>
-            <BouncingPreloader
-                icons={icons}
-                leftDistance={-50}
-                rightDistance={-170}
-                speed={1100}
-            />
+        <View style={tailwind("flex flex-col items-center")}>
+            <View style={styles.container}>
+                <BouncingPreloader
+                    icons={icons}
+                    leftDistance={-50}
+                    rightDistance={-170}
+                    speed={1100}
+                />
+            </View>
+            <Text style={[tailwind("mt-10 text-lg font-semibold"), styles.text]}>Loading...</Text>
         </View>
     );
 }
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
         alignItems: "center",
         justifyContent: "center",
-    }
+        marginTop: 350
+    },
+    text: {
+        color: '#bfbfcf',
+    },
 });
 
 export default Loading;
